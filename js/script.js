1cc490e10f6da0f362e9ed5fe500743bacd7f39d
KFURU6 <my_time_is_now@i.softbank.jp>
2022-05-02 17:02:20 +0900

    ファイル整理

--- js/script.js
+++ js/script.js
@@ -106,34 +106,4 @@ $(function () {
             }
         }
     });
-
-
-    //   const swiper = new Swiper('.swiper', {
-    //     // Optional parameters
-    //     direction: 'vertical',
-    //     loop: true,
-      
-    //     // If we need pagination
-    //     pagination: {
-    //       el: '.swiper-pagination',
-    //     },
-      
-    //     // Navigation arrows
-    //     navigation: {
-    //       nextEl: '.swiper-button-next',
-    //       prevEl: '.swiper-button-prev',
-    //     },
-      
-    //     // And if we need scrollbar
-    //     scrollbar: {
-    //       el: '.swiper-scrollbar',
-    //     },
-    //   });
-
 });
-
-// $(document).ready(function(){
-//     $('.results__contents').slick({
-//     //   setting-name: setting-value
-//     });
-//   });
